989c8992eaf4e00586a07e2f8a0121b5274177a1
Matthew Lam <matt@mryanlam.com>
2020-10-05 19:30:29 -0700

    pass the role not the id

--- solution.py
+++ solution.py
@@ -16,7 +16,7 @@ async def on_member_join(member):
     # Assigns role on join
     print("{} joined".format(member))
     guest_role = member.guild.get_role(guest_role_id)
-    await member.add_roles(guest_role_id, reason="Guest User")
+    await member.add_roles(guest_role, reason="Guest User")
 
 def get_credentials():
     with open("config.txt") as f:
